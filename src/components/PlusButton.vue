00aa2aebb3986cc25318da306cd36251a0537e9c
Marco Lugo Rodriguez <marco.lugo-rodriguez.1@ulaval.ca>
2019-11-16 20:51:40 -0500

    add tooltip to add-to-playlist button to make UI friendlier

--- src/components/PlusButton.vue
+++ src/components/PlusButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a id="plus-button">
+  <a id="plus-button" class="tooltip" data-tooltip="Add to playlist">
     <img src="../images/plus.svg" alt="Plus button" />
   </a>
 </template>
@@ -15,6 +15,7 @@ export default {
   display: inline;
   padding-right: 1%;
   align-items: center;
+  cursor: pointer;
 }
 
 #plus-button > img {
@@ -22,4 +23,22 @@ export default {
   height: 22px;
   vertical-align: middle;
 }
+
+a.tooltip {
+  position: relative ;
+}
+
+a.tooltip:hover::after {
+  content: attr(data-tooltip);
+  position: absolute ;
+  top: 1.1em;
+  left: 1em;
+  min-width: 13px;
+  border: 1px #636262 solid;
+  border-radius: 15px;
+  padding: 7px;
+  color: black;
+  background-color: #ffffff ;
+  z-index: 1;
+} 
 </style>
