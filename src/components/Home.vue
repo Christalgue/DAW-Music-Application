da813cc0b33704ae84e98cd79192d1d2f3aec25c
Marco Lugo Rodriguez <marco.lugo-rodriguez.1@ulaval.ca>
2019-12-14 16:48:47 -0500

    add extra feature: artist's bio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,22 @@
     <div class="main-title bordered">
       <h1>Welcome!</h1>
     </div>
-    <div class="main-title bordered">
-      
+    <div class="title bordered">
+      <h2>Your artists :</h2>
+      <ul>
+        <li>
+          <router-link class="hyperlink-decoration" :to="{ name: 'Artist', params: { artistId: 32940 } }">
+            Michael Jackson
+          </router-link>
+        </li>
+        <li>
+          <router-link class="hyperlink-decoration" :to="{ name: 'Artist', params: { artistId: 2820865 } }">
+            The Offspring
+          </router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
-<script>
-
-</script>
-
-<style scoped>
-
-</style>
+<style></style>
