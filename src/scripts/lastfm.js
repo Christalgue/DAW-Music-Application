629392f04bb00349a0b535049254cf9483ba770b
Marco Lugo Rodriguez <marco.lugo-rodriguez.1@ulaval.ca>
2019-12-14 17:23:27 -0500

    add extra feature: show similar artists

--- src/scripts/lastfm.js
+++ src/scripts/lastfm.js
@@ -1,15 +1,24 @@
 const BASE_URL = "http://ws.audioscrobbler.com/2.0/?";
-const LAST_FM_API_KEY = "9fee80eb0f0bf9b630759f422d91e45e"
+const LAST_FM_API_KEY = "9fee80eb0f0bf9b630759f422d91e45e";
 
-export const getBiography = artistName => {
+export const getArtistLastFM = artistName => {
   let queryUrl = `${BASE_URL}method=artist.getinfo&artist=${artistName}&api_key=${LAST_FM_API_KEY}&format=json`;
-  console.log(queryUrl);
   return fetch(queryUrl)
     .then(response => response.json())
     .then(artistData => {
-      return artistData.artist.bio;
+      return artistData.artist;
     })
     .catch(err => {
       console.log(err.message);
     });
+};
+
+export const extractSimilarArtistsNames = data => {
+  let result = [];
+
+  for (let artist of data) {
+    result.push(artist.name);
+  }
+
+  return result;
 };
