504bdef4440e6a60bfecda398d8e722fd609848c
Marco Lugo Rodriguez <marco.lugo-rodriguez.1@ulaval.ca>
2019-12-14 19:03:45 -0500

    improve code quality (fix linter warnings)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,52 +3,49 @@
     <form autocomplete="off" @submit.prevent="login">
       <div class="loginbox">
         <label for="email"><b>Email</b></label>
-        <input v-model="email" type="email" v-bind:class="{ loginError: isLoginError }" @keydown="clearError">
+        <input v-model="email" type="email" v-bind:class="{ loginError: isLoginError }" @keydown="clearError" />>
 
         <label for="password"><b>Password</b></label>
-        <input v-model="password" type="password">
-            
+        <input v-model="password" type="password" />
         <button type="submit">Login</button>
       </div>
     </form>
   </div>
 </template>
 
-
 <script>
-import Cookies from 'js-cookie';
-import { sendPOST } from '../backend/helpers';
+import Cookies from "js-cookie";
+import { sendPOST } from "../backend/helpers";
 
 export default {
   name: "Login",
-  data () {
+  data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       isLoginError: false
-    }
+    };
   },
   methods: {
-    login () {
-      let data = {email: this.email, password: this.password};
-      sendPOST('/api/login', data)
-      .then(response => {
+    login() {
+      let data = { email: this.email, password: this.password };
+      sendPOST("/api/login", data).then(response => {
         if (response.status == 400) {
           this.email = "Wrong combination, try again!";
           this.password = "";
           this.isLoginError = true;
         } else {
           response.json().then(response => {
-            let expirationInDays = 1 / 2;  // TODO: change expiration (currently 12 hours)
-            Cookies.set('token', response.token, {
-                expires: expirationInDays
+            let expirationInDays = 1 / 2;
+            Cookies.set("token", response.token, {
+              expires: expirationInDays
             });
-            this.$router.go(-1);  // now that user did log in, go where the user came from before the login
+            this.$router.go(-1); // now that user did log in, go where the user came from before the login
           });
         }
       });
     },
-    clearError () {
+    clearError() {
       if (this.isLoginError) {
         this.email = "";
         this.password = "";
@@ -59,9 +56,9 @@ export default {
 };
 </script>
 
-
 <style scoped>
-input[type=email], input[type=password] {
+input[type="email"],
+input[type="password"] {
   width: 100%;
   padding: 12px 20px;
   margin: 9px 0;
